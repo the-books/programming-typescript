cdc2df3d3ed97de88bd7cc4e30189bccbc488b33
GimYoungPhil <youngphil.gim@gmail.com>
2020-05-28 11:03:53 +0900

    init

---
+++ src/ch02/index.ts
@@ -0,0 +1,15 @@
+let a = 1 + 2;
+let b = a + 3;
+let c = {
+  apple: a,
+  banana: b,
+};
+let d = c.apple * 4;
+
+let e = [1, 2];
+let f = { a: 1, b: 2 };
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
