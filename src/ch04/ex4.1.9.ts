9bdb005c9720af9d74e7780bc8618491433702bf
GimYoungPhil <youngphil.gim@gmail.com>
2020-06-02 17:03:03 +0900

    fixed

--- src/ch04/ex4.1.9.ts
+++ src/ch04/ex4.1.9.ts
@@ -1,24 +1,39 @@
-
 // 단축형 호출 시그니처
 // type Log = (message: string, userId?: string) => void;
 
 // 전체 호출 시그니처
-type Log = {
-  (message: string, userId?: string): void,
+// type Log = {
+//   (message: string, userId?: string): void,
+// };
+
+
+type Reservation = {
+  price: number;
 };
-
-
-type Reservation {
-
-}
-
 
 type Reserve = {
   (from: Date, to: Date, destinations: string): Reservation,
   (from: Date, destinations: string): Reservation,
 };
 
-let reserve: Reserve = (from, to, destination) => {
-
+const reserve: Reserve = (
+  from: Date,
+  toOrDestination: Date | string,
+  destination?: string,
+) => {
+  let price = 0;
+  if (toOrDestination instanceof Date && destination !== undefined) {
+    // 편도 여행 예약
+    price = 15_000;
+  } else if (typeof toOrDestination === 'string') {
+    // 왕복 여행 예약
+    price = 25_000;
+  }
+  return {
+    price,
+  };
 };
+
+reserve(new Date('2020-06-01'), new Date('2020-06-06'), '제주도');
+
 export {};
