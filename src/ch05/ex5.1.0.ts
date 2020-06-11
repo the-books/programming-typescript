0d61f84d7f106633692a26eec772cd268b1c45ea
GimYoungPhil <youngphil.gim@gmail.com>
2020-06-11 16:23:02 +0900

    fixed

--- src/ch05/ex5.1.0.ts
+++ src/ch05/ex5.1.0.ts
@@ -1,20 +1,35 @@
 type Color = 'Black' | 'White';
-type Rank = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h';
-type File =  1  |  2  |  3  |  4  |  4  |  5  |  6  |  7;
+type File = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H';
+type Rank = 1 | 2 | 3 | 4 | 4 | 5 | 6 | 7;
+
+type Distance = {
+  rank: number;
+  file: number;
+};
 
 class Game {}
 
 // tslint:disable-next-line: max-classes-per-file
 class Position {
+
   constructor(
     private rank: Rank,
     private file: File,
   ) {}
+
+  distanceFrom(position: Position): Distance {
+    return {
+      rank: Math.abs(position.rank - this.rank),
+      file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0)),
+    };
+  }
 }
 
 // tslint:disable-next-line: max-classes-per-file
 abstract class Piece {
+
   protected position: Position;
+
   constructor(
     private readonly color: Color,
     rank: Rank,
@@ -22,18 +37,34 @@ abstract class Piece {
   ) {
     this.position = new Position(rank, file);
   }
+
+  moveTo(postion: Position) {
+    this.position = this.position;
+  }
+
+  abstract canMoveTo(position: Position): boolean;
 }
 
 // tslint:disable-next-line: max-classes-per-file
-class King extends Piece {}
+class King extends Piece {
+  canMoveTo(position: Position) {
+    const distance = this.position.distanceFrom(position);
+    return distance.rank < 2 && distance.file < 2;
+  }
+}
 
 // tslint:disable-next-line: max-classes-per-file
-class Queen extends Piece {}
+class Queen extends Piece {
+  canMoveTo() {
 
-const king = new King('Black', 'a', 1);
+  }
+}
+
+const king = new King('Black', 'A', 1);
 console.log(king);
 
-const gueen = new Queen('White', 'b', 1);
+const gueen = new Queen('White', 'B', 1);
 console.log(gueen);
+
 
 export {};
